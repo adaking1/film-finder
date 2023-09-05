7c4edc14e7fdc5f57534a36d5d4d0f0f527cd4e9
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-09-05 13:17:01 -0400

    finished adding js for youtube's api functionalities

--- script.js
+++ script.js
@@ -1,107 +1,94 @@
 var searchText = document.querySelector("#search");
+var searchButton = document.querySelector("#search-button");
+var ytPlayer = document.querySelector("#player");
+var videoId
 
 function searchVideos(video) {
-    // var key = "AIzaSyDefjXnp5D479WhFVN5_4WXCEdQ79NLuRU";
-    var ytUrlPrefix = "";
+  if (video.includes(" ")) {
+    video = video.replace(" ", "+");
+  }
+    video = video.toLowerCase();
+    var key = "&key=AIzaSyDefjXnp5D479WhFVN5_4WXCEdQ79NLuRU";
+    var ytUrlPrefix = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=";
     var videoName = video + "+trailer"
 
+    console.log(video);
     console.log(ytUrlPrefix + videoName );
 
-    fetch ("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=dio&key=AIzaSyDefjXnp5D479WhFVN5_4WXCEdQ79NLuRU")
+    fetch (ytUrlPrefix + videoName + key)
     .then (function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+        console.log(data.items[0].id.videoId);
+        videoId = data.items[0].id.videoId;
+        console.log(videoId);
+        if (player){
+          player.destroy();
+          console.log("DESTROY");
+          videoPlay(videoId);
+        }
+        else {
+          iFrameAPIcall();
+        }
     })
     
-// //     var results = YouTube.Search.list('id,snippet', {q: video + " trailer", maxResults:1});
-
-// //     .then 
-
-// //     for(var i in results.items) {
-// //         var item = results.items[i];
-// //         Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-// //       }
+    
 }
 
-
-// function authenticate() {
-//     return gapi.auth2.getAuthInstance()
-//         .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
-//         .then(function() { console.log("Sign-in successful"); },
-//               function(err) { console.error("Error signing in", err); });
-//   }
-//   function loadClient() {
-//     gapi.client.setApiKey("YOUR_API_KEY");
-//     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-//         .then(function() { console.log("GAPI client loaded for API"); },
-//               function(err) { console.error("Error loading GAPI client for API", err); });
-//   }
-//   // Make sure the client is loaded and sign-in is complete before calling this method.
-//   function execute() {
-//     return gapi.client.youtube.search.list({
-//       "part": [
-//         "snippet"
-//       ],
-//       "order": "viewCount",
-//       "q": "Superman trailer",
-//       "type": [
-//         "video"
-//       ],
-//       "videoDefinition": "high"
-//     })
-//         .then(function(response) {
-//                 // Handle the results here (response.result has the parsed body).
-//                 console.log("Response", response);
-//               },
-//               function(err) { console.error("Execute error", err); });
-//   }
-//   gapi.load("client:auth2", function() {
-//     gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-//     });
-
-
-
-
+function iFrameAPIcall(){
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+  var tag = document.createElement('script');
+  var firstScriptTag = document.getElementsByTagName('script')[0];
 
-tag.src = "https://www.youtube.com/iframe_api?key=AIzaSyDefjXnp5D479WhFVN5_4WXCEdQ79NLuRU";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  tag.src = "https://www.youtube.com/iframe_api?key=AIzaSyDefjXnp5D479WhFVN5_4WXCEdQ79NLuRU";
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+}
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
+
 function onYouTubeIframeAPIReady() {
+  console.log("Ready");
+  console.log(videoId);
+  videoPlay(videoId);
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+  var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+
+
+var player;
+function videoPlay(id){
+  console.log(player);
+  console.log(id);
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: '76WeEuR0qk4',
+    videoId: id,
     playerVars: {
       'playsinline': 1
     },
     events: {
-      'onReady': onPlayerReady,
+      // 'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
     }
   });
 }
 
-function onPlayerReady(event) {
-    event.target.playVideo();
-  }
 
-var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-
-searchVideos("Superman");
+searchButton.addEventListener("click", function(){
+  searchVideos(searchText.value);
+});
